8f84a283754add7383935ec63d497b4648bbdbbe
Gavin <90148140+AperanoGavin@users.noreply.github.com>
2023-11-12 16:19:15 +0100

    api platerform symfony ok

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -1,29 +1,44 @@
 <?php
 
+
 namespace App\Entity;
 
 use App\Repository\CustomerRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: CustomerRepository::class)]
+#[ApiResource(
+    normalizationContext: ['groups' => ['customer:item', 'customer:list']],
+    denormalizationContext: ['groups' => ['customer:item', 'customer:write']]
+)]
 class Customer
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['customer:item', 'customer:list'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['customer:item', 'customer:write'])]
     private ?string $firstname = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['customer:item', 'customer:write'])]
     private ?string $lastname = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['customer:item', 'customer:write'])]
     private ?string $email = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['customer:item', 'customer:write'])]
     private ?string $password = null;
+
 
     public function getId(): ?int
     {
